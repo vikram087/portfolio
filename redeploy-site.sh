77417c173e9bfb7c3ce34f689345ff5fe01609d0
root <vikram.penumarti@gmail.com>
2025-07-15 05:26:42 +0000

    new redeploy

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-tmux kill-server
 cd portfolio
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux new-session -d -s flask-server -c /root/portfolio \; send-keys "source python3-virtualenv/bin/activate" Enter \; send-keys "flask run --host=0.0.0.0" Enter
-
+systemctl daemon-reload
+systemctl restart myportfolio
